ee077b240c574d827bf8c889e4b049f5242622f1
ghiath-alabed <030121069@std.izu.edu.tr>
2023-05-19 17:30:25 +0300

    some new updates

--- biography.php
+++ biography.php
@@ -27,7 +27,7 @@
               <a href="#" class="navbar-link" title="Anasayfa" target="_self">
                 Anasayfa
               </a>  
-              <a href="worldCup.php" class="navbar-link" title="Dünya kupası" target="_self">
+              <a href="#" class="navbar-link" title="Dünya kupası" target="_self">
                 Dünya kupası
               </a>
               <a href="#" class="navbar-link" title="Oyuncuların Hayat Özti" target="_self"> <!-- _blank -->
@@ -101,67 +101,3 @@
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </html>
-
-
-
-
-<!-- <?php
-                // include_once 'includes/dbh.inc.php';
-                // session_start();
-                // if (isset($_SESSION["id"])) {
-                //   $id = $_SESSION["id"];
-                //   echo "<script>console.log('$id');</script>";
-                //   $sql = "SELECT * FROM User WHERE id = '$id'";
-                //   $result = mysqli_query($conn, $sql);
-                //   if (mysqli_num_rows($result) > 0) {
-                //     // User exists, continue and navigate to the index
-                //     $row = mysqli_fetch_assoc($result);
-                //     $email = $row['email'];
-                //     $username = $row['username'];
-                //     echo "<script src=\"js/Proje.js\"></script>";
-                //     // Here add header html
-                //     echo "<a class=\"Login-button\" onclick=\"logout()\">
-                //         Logout '$username'
-                //     </a>";
-                //   }
-                // } else {
-                //     echo '<a class="Login-button" href="login.php#" title="login-page">
-                //       <ion-icon name="person-circle-outline"></ion-icon>
-                //         Login / Register
-                //       <ion-icon name="person-add-outline"></ion-icon>
-                //     </a>';
-                // }
-              ?> -->
-
-
-
-<!-- 
-<div class="container">
-  <div class="news-section">
-    <div class="news-article">
-      <img src="https://via.placeholder.com/390x260.png" alt="News Image" />
-      <h2>Future of Transportation - Hyperloop</h2>
-      <p>
-        The world is about to witness a revolution in transportation, as
-        Hyperloop projects continue to advance.
-      </p>
-    </div>
-    <div class="news-article">
-      <img src="https://via.placeholder.com/390x260.png" alt="News Image" />
-      <h2>Space Tourism on the Rise</h2>
-      <p>
-        With recent advancements from the likes of SpaceX and Blue Origin,
-        space tourism is quickly becoming a reality.
-      </p>
-    </div>
-    <div class="news-article">
-      <img src="https://via.placeholder.com/390x260.png" alt="News Image" />
-      <h2>Solar Power Breakthroughs</h2>
-      <p>
-        Researchers are developing innovative materials to increase the
-        efficiency of solar power generation.
-      </p>
-    </div>
-    <!-- Add more news articles below -->
-  <!-- </div>
-</div> -->
